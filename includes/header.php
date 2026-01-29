13a0801dd5bd42b5d885937d0ec6d01ff2868458
Raoul <raoulgatepin@gmail.com>
2026-01-30 00:09:28 +0100

    code Matomo

--- includes/header.php
+++ includes/header.php
@@ -25,6 +25,23 @@
             font-family: 'Source Sans 3', sans-serif;
         }
     </style>
+
+    <!-- Matomo -->
+    <script>
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="https://monmatomo.com/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '38']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img referrerpolicy="no-referrer-when-downgrade" src="https://monmatomo.com/matomo.php?idsite=38&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Matomo Code -->
 </head>
 <body>
     <!-- Bandeau tricolore -->
